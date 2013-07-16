57b87e151fe6269fb421736bcf773632da538f54
makeapps-PC4 <makeapps-PC4@makeapps-PC4-PC>
2013-07-16 14:24:56 +0800

    css + user module

--- application/views/header.php
+++ application/views/header.php
@@ -1,111 +1,132 @@
+
 <!DOCTYPE html>
-<base href="<?php echo base_url();?>">
+<base href="<?php echo base_url(); ?>">
 <html lang="en">
-<head>
-	<!--
-		Charisma v1.0.0
+    <head>
+        <!--
+                Charisma v1.0.0
 
-		Copyright 2012 Muhammad Usman
-		Licensed under the Apache License v2.0
-		http://www.apache.org/licenses/LICENSE-2.0
+                Copyright 2012 Muhammad Usman
+                Licensed under the Apache License v2.0
+                http://www.apache.org/licenses/LICENSE-2.0
 
-		http://usman.it
-		http://twitter.com/halalit_usman
-	-->
-	<meta charset="utf-8">
-	<title>Online Law Firm System</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
-	<meta name="author" content="Muhammad Usman">
+                http://usman.it
+                http://twitter.com/halalit_usman
+        -->
+        <meta charset="utf-8">
+        <title>Online Law Firm System</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
+        <meta name="author" content="Muhammad Usman">
 
-	<!-- The styles -->
-	<link id="bs-css" href="  css/bootstrap-cerulean.css" rel="stylesheet">
-	<style type="text/css">
-	  body {
-		padding-bottom: 40px;
-	  }
-	  .sidebar-nav {
-		padding: 9px 0;
-	  }
-	</style>
-	<link href="  css/bootstrap-responsive.css" rel="stylesheet">
-	<link href="  css/charisma-app.css" rel="stylesheet">
-	<link href="  css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
-	<link href='  css/fullcalendar.css' rel='stylesheet'>
-	<link href='  css/fullcalendar.print.css' rel='stylesheet'  media='print'>
-	<link href='  css/chosen.css' rel='stylesheet'>
-	<link href='  css/uniform.default.css' rel='stylesheet'>
-	<link href='  css/colorbox.css' rel='stylesheet'>
-	<link href='  css/jquery.cleditor.css' rel='stylesheet'>
-	<link href='  css/jquery.noty.css' rel='stylesheet'>
-	<link href='  css/noty_theme_default.css' rel='stylesheet'>
-	<link href='  css/elfinder.min.css' rel='stylesheet'>
-	<link href='  css/elfinder.theme.css' rel='stylesheet'>
-	<link href='  css/jquery.iphone.toggle.css' rel='stylesheet'>
-	<link href='  css/opa-icons.css' rel='stylesheet'>
-	<link href='  css/uploadify.css' rel='stylesheet'>
+        <!-- The styles -->
+        <link id="bs-css" href="  css/bootstrap-cerulean.css" rel="stylesheet">
+        <style type="text/css">
+            body {
+                padding-bottom: 40px;
+            }
+            .sidebar-nav {
+                padding: 9px 0;
+            }
+        </style>
+        <link href="  css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="  css/charisma-app.css" rel="stylesheet">
+        <link href="  css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
+        <link href='  css/fullcalendar.css' rel='stylesheet'>
+        <link href='  css/fullcalendar.print.css' rel='stylesheet'  media='print'>
+        <link href='  css/chosen.css' rel='stylesheet'>
+        <link href='  css/uniform.default.css' rel='stylesheet'>
+        <link href='  css/colorbox.css' rel='stylesheet'>
+        <link href='  css/jquery.cleditor.css' rel='stylesheet'>
+        <link href='  css/jquery.noty.css' rel='stylesheet'>
+        <link href='  css/noty_theme_default.css' rel='stylesheet'>
+        <link href='  css/elfinder.min.css' rel='stylesheet'>
+        <link href='  css/elfinder.theme.css' rel='stylesheet'>
+        <link href='  css/jquery.iphone.toggle.css' rel='stylesheet'>
+        <link href='  css/opa-icons.css' rel='stylesheet'>
+        <link href='  css/uploadify.css' rel='stylesheet'>
 
-	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
-	<!--[if lt IE 9]>
-	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
+        <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
 
-	<!-- The fav icon -->
-	<link rel="shortcut icon" href="  img/favicon.ico">
-		
-</head>
+        <!-- The fav icon -->
+        <link rel="shortcut icon" href="  img/favicon.ico">
 
-<body>
-	<?php if(!isset($no_visible_elements) || !$no_visible_elements)	{ ?>
-	<!-- topbar starts -->
-	<div class="navbar">
-		<div class="navbar-inner">
-			<div class="container-fluid">
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".top-nav.nav-collapse,.sidebar-nav.nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-								
-				<!-- user dropdown starts -->
-				<div class="btn-group pull-right" >
-					<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-						<i class="icon-user"></i><span class="hidden-phone">ChanPrimeBig</span>
-						<span class="caret"></span>
-					</a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Profile</a></li>
-						<li class="divider"></li>
-						<li><a href="login.html">Logout</a></li>
-					</ul>
-				</div>
-				<!-- user dropdown ends -->
-				
-				
-			</div>
-		</div>
-	</div>
-	<!-- topbar ends -->
-	<?php } ?>
-	<div class="container-fluid">
-		<div class="row-fluid">
-		<?php if(!isset($no_visible_elements) || !$no_visible_elements) { ?>
-		
-			<!-- left menu starts -->
-			<div class="span2 main-menu-span">
-				<div class="well nav-collapse sidebar-nav">
-					<ul class="nav nav-tabs nav-stacked main-menu">
-						<li class="nav-header hidden-tablet">Main</li>
-						<li><a class="ajax-link" href="index.html"><i class="icon-home"></i><span class="hidden-tablet"> Home</span></a></li>
-						<li><a class="ajax-link" href="templates.html"><i class="icon-list-alt"></i><span class="hidden-tablet"> Templates</span></a></li>
-						<li><a class="ajax-link" href="accounts.html"><i class="icon-user"></i><span class="hidden-tablet"> Accounts</span></a></li>
-					</ul>					
-				</div><!--/.well -->
-			</div><!--/span-->
-			<!-- left menu ends -->
-			
-			
-			
-			<div id="content" class="span10">
-			<!-- content starts -->
-			<?php } ?>
+    </head>
+
+    <body>
+        
+        <?php if (!isset($no_visible_elements) || !$no_visible_elements) { ?>
+            <!-- topbar starts -->
+            <div class="navbar">
+                <div class="navbar-inner">
+                    <div class="container-fluid">
+                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".top-nav.nav-collapse,.sidebar-nav.nav-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </a>
+
+                        <!-- user dropdown starts -->
+                        <div class="btn-group pull-right" >
+                            <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="icon-user"></i><span class="hidden-phone">ChanPrimeBig</span>
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Profile</a></li>
+                                <li class="divider"></li>
+                                <li><a href="login.html">Logout</a></li>
+                            </ul>
+                        </div>
+                        <!-- user dropdown ends -->
+
+
+                    </div>
+                </div>
+            </div>
+            <!-- topbar ends -->
+
+        <?php } ?>
+        <div class="container-fluid">
+
+            <div class="row-fluid">
+
+                <?php if (!isset($no_visible_elements) || !$no_visible_elements) { ?>
+
+                    <!-- left menu starts -->
+                    <div class="span2 main-menu-span">
+                        <div class="well nav-collapse sidebar-nav">
+                            <ul class="nav nav-tabs nav-stacked main-menu">
+                                <li class="nav-header hidden-tablet">Main</li>
+                                <li><a class="ajax-link" href="index.html"><i class="icon-home"></i><span class="hidden-tablet"> Home</span></a></li>
+                                <li><a class="ajax-link" href="templates.html"><i class="icon-list-alt"></i><span class="hidden-tablet"> Templates</span></a></li>
+                                <li><a class="ajax-link" href="<?php base_url() ?>user"><i class="icon-user"></i><span class="hidden-tablet"> Accounts</span></a></li>
+                            </ul>
+
+                        </div><!--/.well -->
+
+                    </div><!--/span-->
+                    <!-- left menu ends -->
+
+
+
+                    <div id="content" class="span10">
+                        <!-- content starts -->
+                        <div class="row-fluid">
+                            <div class="span12">
+                                <ul class="breadcrumb">
+                                    <li>
+                                        <a href="#">Home</a> <span class="divider">/</span>
+                                    </li>
+                                    <li>
+                                        <a href="#">Blank</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                       
+                    <?php } ?>
+                   
