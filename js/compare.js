137b857606263377a72381de8ae75dd186c4e9a8
makeapps-PC4 <makeapps-PC4@makeapps-PC4-PC>
2013-07-16 23:24:26 +0800

    document module + user module

---
+++ js/compare.js
@@ -0,0 +1,15 @@
+<script>
+   function compare()
+     {
+         
+        if (user_create==true)
+           {
+             x="Good day";
+           }
+       else
+           {
+             x="Good evening";
+           } 
+        
+     }
+</script>
