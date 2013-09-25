7585903253a3e8f96b7df29d8c0ee87847411197
Administrator <Administrator@WIN-IF74S9L0QIS>
2013-09-25 22:49:06 +0800

    login

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'http://lawfirm.gopagoda.com/';
+$cfg['Servers'][$i]['host'] = 'tunnel.pagodabox.com';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 /* Select mysql if your server does not have mysqli */
